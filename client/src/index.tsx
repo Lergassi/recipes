696a0a084c42fccbdcf685dd8238f18e81b1d580
Lergassi <lergassi.dev@yandex.ru>
2023-09-16 21:25:38 +0500

    - webpack prod. Начало.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,6 +7,10 @@ let api = new Api(
     process.env.APP_API_URL,
 );
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
